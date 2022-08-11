3c354c4ef618b0e6389930dc25e734aa02801d61
Athif Ahamed <81356981+athifahamedj@users.noreply.github.com>
2022-08-11 18:04:38 +0530

    basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="padding-css">
+				<label>Name : </label>
+				<input type="text" className="form-control" />
+			</div>
+			<div className="padding-css">
+				<label>Age : </label>
+				<input type="text" className="form-control" />
+			</div>
+			<div className="btn-css">
+				<button className="btn" type="submit">
+					Submit
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;

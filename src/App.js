9873208b98af2a536355e97d0d41d6aaa1d9bd02
Athif Ahamed <81356981+athifahamedj@users.noreply.github.com>
2022-08-13 10:41:14 +0530

    scratch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,4 @@
-// import logo from "./logo.svg";
-import "./App.css";
+import react from "react";
 
-function App() {
-	return (
-		<div className="App">
-			<div className="padding-css">
-				<label>Name : </label>
-				<input type="text" className="form-control" />
-			</div>
-			<div className="padding-css">
-				<label>Age : </label>
-				<input type="text" className="form-control" />
-			</div>
-			<div className="btn-css">
-				<button className="btn" type="submit">
-					Submit
-				</button>
-			</div>
-		</div>
-	);
-}
-
+return <div>test</div>;
 export default App;
